e93783191a10f4dc44002f123937620b1948e6b4
Daizhibo1219 <48171768+Daizhibo1219@users.noreply.github.com>
2022-01-21 17:52:33 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+function setup() {
+	createCanvas(800,800); // make an HTML canvas element width x height pixels
+}
+
+function draw() {
+	background(225);
+	textSize(40);
+	translate(width / 2, height / 2);
+	text('12', 0, -250)
+	text('3', 270, 13)
+	text('6', 0, 285)
+	text('9', -270, 13)
+	textAlign(CENTER);
+	text(hour() + ':' + minute() + ':' + second(), 0, 350)
+	push();
+	stroke('purple');
+	strokeWeight(10);
+	noFill();
+	circle(0, 0, 600)
+	pop();
+	push();
+	rotate( second() * PI / 30.0);
+	stroke('blue')
+	strokeWeight(10);
+	line(0, 0, 0, -200);
+	pop();
+	push();
+	rotate( minute() * PI / 30.0);
+	stroke('green')
+	strokeWeight(10);
+	line(0, 0, 0, -150);
+	pop();
+	push();
+	rotate( (hour()%12) * PI / 6.0);
+	stroke('black')
+	strokeWeight(10);
+	line(0, 0, 0, -100);
+	pop();
+	push();
+	stroke('white')
+	strokeWeight(5)
+	point(0,0)
+	pop();
+}
+
